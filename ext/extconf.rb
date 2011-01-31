8ba5ab020294ce6c9074cc9d12a7b126996fba03
Joshua Krall <joshuakrall@pobox.com>
2011-01-31 00:55:06 -0600

    Fixing build problem, cleanup, & .gitignore

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,7 @@
 require 'mkmf'
 
-dir_config('GeoIP')
-have_library('GeoIP', 'GeoIP_new')
-
-create_makefile('net/geoip')
+# dir_config('GeoIP')
+# have_library('GeoIP', 'GeoIP_new')
+with_cflags('-DGEOIPDATADIR=\"/usr/local/share/GeoIP\"') do
+  create_makefile('net/geoip')
+end
