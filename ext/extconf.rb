78c44d300ae44e9b43025e0b4722024bd0db0e08
Joshua Krall <joshuakrall@pobox.com>
2011-01-31 16:03:35 -0600

    fixing makefile

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,5 +3,5 @@ require 'mkmf'
 # dir_config('GeoIP')
 # have_library('GeoIP', 'GeoIP_new')
 with_cflags('-DGEOIPDATADIR=\"/usr/local/share/GeoIP\"') do
-  create_makefile('net/geoip')
+  create_makefile('max_mind/net/geoip')
 end
